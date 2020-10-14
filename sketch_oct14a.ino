30908d8097371d0130c16683de395a9636edb78f
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2020-10-14 21:39:39 +0700

    first commit

---
+++ sketch_oct14a.ino
@@ -0,0 +1,18 @@
+boolean okToPrint = true;
+int const den = 12;
+void setup(){
+Serial.begin(9600);
+pinMode(8, INPUT_PULLUP); 
+pinMode(den,OUTPUT);
+}
+ 
+void loop(){
+  int sensorVal = digitalRead(8); 
+  if (sensorVal == HIGH) { //nếu chưa nhấn
+    digitalWrite(den, LOW); // tắt đèn
+  }  else {  // nếu đã nhấn 
+    digitalWrite(den, HIGH);// bật đèn
+    Serial.println("okToPrint");
+    delay(500);
+  }
+}
